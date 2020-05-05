c4249f5052ecc8c6a0ddc94eb22157a2daaad139
dulcetiseduce1 <swinnaiscool@gmail.com>
2020-05-05 19:47:58 +0200

    spazi

--- src/Distributore.java
+++ src/Distributore.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 
 public class Distributore {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        double x,y,z;
+        double x, y, z;
         String prezzo;
         String rifornimento;
         String euro;
@@ -11,11 +11,15 @@ public class Distributore {
         PompaBenzina pb1 = new PompaBenzina();
         prezzo = JOptionPane.showInputDialog("Inserire il prezzo per litro: ", "");
         y = Double.valueOf(prezzo);
+
         rifornimento = JOptionPane.showInputDialog("Inserire i litri per il rifornimento: ", "");
         x = Double.valueOf(rifornimento);
+
         pb1.rifornisci(x);
         euro = JOptionPane.showInputDialog("Importo per l'acquisto: ", "");
         z = Double.valueOf(euro);
+
         pb1.vendi(z, x, y);
+
     }
 }
--- src/PompaBenzina.java
+++ src/PompaBenzina.java
@@ -3,43 +3,56 @@ import javax.swing.*;
 public class PompaBenzina {
 
     double deposito;
-    private double maxd = 50000;
+    private final double maxd = 50000;
 
-    public PompaBenzina(){
+    public PompaBenzina()
+    {
         deposito = 0;
     }
 
-    public void rifornisci(double q){
+    public void rifornisci(double q)
+    {
         double spazio;
         spazio = maxd - deposito;
 
-        if(spazio>=q){
+        if (spazio >= q) {
+
             deposito = deposito + q;
             System.out.println("Deposito da inserire " + q);
             System.out.println("Deposito " + deposito);
-            JTextField text = new JTextField("Vi sono in deposito "+ deposito +" litri.");
+            JTextField text = new JTextField("Vi sono in deposito " + deposito + " litri.");
             JOptionPane.showMessageDialog(null, text.getText());
-        }else{
+        }
+
+        else {
+
             JTextField text = new JTextField("Non è possibile rifornire il distributore.");
             JOptionPane.showMessageDialog(null, text.getText());
         }
     }
 
-    public void vendi(double z1, double v, double z){
-        if(maxd>v)
-        {
+    public void vendi(double z1, double v, double z)
+    {
+        if (maxd > v) {
             System.out.println("Prezzo: " + z);
         }
-        z = z1*z;
-        if(maxd>v){
+
+        z = z1 * z;
+
+        if (maxd > v) {
+
             JTextField text = new JTextField("Hai comprato " + z + " di litri di benzina.");
-            System.out.print("costo totale:"+" "+z);
+            System.out.print("costo totale:" + " " + z);
             JOptionPane.showMessageDialog(null, text.getText());
 
-        }else{
+              }
+
+        else {
+
             JTextField text = new JTextField("Non ci sono litri sufficienti nel distributore.");
             JOptionPane.showMessageDialog(null, text.getText());
-        }
+
+             }
     }
 
 }
