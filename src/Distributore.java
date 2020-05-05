e2d65e666fa27ba9b196d9a4a15d11039246db27
Ivan Migliore <migliore.ivan.27@gmail.com>
2020-05-05 18:55:33 +0200

    æ

--- src/Distributore.java
+++ src/Distributore.java
@@ -3,24 +3,20 @@ import javax.swing.*;
 public class Distributore {
     public static void main(String[] args){
 
-
         double x,y,z;
         String prezzo;
         String rifornimento;
         String euro;
 
-
         PompaBenzina pb1 = new PompaBenzina();
         prezzo = JOptionPane.showInputDialog("Inserire il prezzo per litro: ", "");
-        y = Double.valueOf(prezzo).doubleValue();
+        y = Double.valueOf(prezzo);
         rifornimento = JOptionPane.showInputDialog("Inserire i litri per il rifornimento: ", "");
-        x = Double.valueOf(rifornimento).doubleValue();
+        x = Double.valueOf(rifornimento);
         pb1.rifornisci(x);
         euro = JOptionPane.showInputDialog("Importo per l'acquisto: ", "");
-        z = Double.valueOf(euro).doubleValue();
-        System.out.println("Prezzo: " + z);
+        z = Double.valueOf(euro);
         pb1.vendi(z, x, y);
-
 
     }
 }
